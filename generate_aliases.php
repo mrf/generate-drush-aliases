307c189b9284adcef739f2ee14e60c9dae6fbdc7
Mark Ferree <markferree@gmail.com>
2014-01-02 16:09:29 -0800

    Major cleanup to work with Acquia repos as well as pass in proper arguments

--- generate_aliases.php
+++ generate_aliases.php
@@ -1,13 +1,38 @@
 <?php
-if($project = opendir('/path/to/sites')) {
-  while (false !== ($entry = readdir($project))) {
-    if('..' !== $entry && '.' !== $entry && is_dir($entry)) {
-      print "//$entry\n";
-      print "\$aliases['" . $entry . "'] = array(\n";
-      print "    'uri' => '" . $entry . ".mysite.com',\n";
-      print "    'root' => '/path/to/sites/" . $entry . "',\n";
-      print "  );\n";
-      print "\n";
-    }
-  }
+$path = $argv[1];
+if(!is_dir($path)) {
+  print $path . ' is not a valid path';
+  exit;
+}
+
+function build_alias($value,$entry) {
+  print "// $value";
+  print PHP_EOL;
+  print "\$aliases['" . $value. "'] = array(" . PHP_EOL;
+  print "  'uri' => '" . $value . ".dev'," . PHP_EOL;
+  print "  'root' =>  $entry," . PHP_EOL;
+  print ");" . PHP_EOL;
+  print PHP_EOL;
+}
+
+if($scanned_directory = array_diff(scandir($path), array('..', '.'))) {
+ foreach($scanned_directory as $value) {
+   $entry = $path . '/' . $value;
+   if(is_dir($entry)) {
+     chdir($entry);
+     $version = shell_exec('drush core-status --format=list version');
+     if($version && $version !== '7.0-dev') {
+       if(file_exists('index.php')) {
+         build_alias($value,$entry);
+       } else if (is_dir('docroot')) {
+         if(file_exists('index.php')) {
+           // Special logic for Acquia repos
+           chdir('docroot');
+           $entry = $entry . '/docroot';
+           build_alias($value,$entry);
+         }
+       }
+     }
+   }
+ }
 }
